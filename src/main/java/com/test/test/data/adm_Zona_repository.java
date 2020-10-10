aa96a988f156582f8a390d238c1f4540072b00f7
alex555roselyn <corado502@gmail.com>
2020-10-10 15:05:28 -0600

    api

---
+++ src/main/java/com/test/test/data/adm_Zona_repository.java
@@ -0,0 +1,12 @@
+package com.test.test.data;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface adm_Zona_repository extends JpaRepository<zonas, Integer>{
+
+	List<zonas> findByMunicipio(int municipio);
+    
+}
---
+++ src/test/java/com/infancia/infancia/InfanciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infancia.infancia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfanciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
